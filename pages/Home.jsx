f9e3447e57295dcbc7815d5dc743ed978c907af7
nicolasdelisee <nicolas.delisee3@gmail.com>
2022-12-09 16:43:25 +0100

    OK

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import "./Home.css";
+import { NavLink } from 'react-router-dom';
 
 export default function Home() {
+  const letterName = ["N", "i", "c", "o", "l", "a", "s", " ", "D", "e", "l", "i", "s", "é", "e"]
   return (
-    <h1>Salut, je suis Jack, Développeur Web</h1>
+    <div className="homepage">
+      <div className="title">
+        {letterName.map((letter) => (
+        (letter === "N" || letter === "D") ? <h1 className="name-letter-maj name-letter-maj-anime">{letter}</h1> : <h1 className="name-letter name-letter-anime">{letter}</h1>
+        
+        ))}
+        </div>
+      <h3 className="job-title job-title-anime">Développeur Junior Front-End</h3>
+      <NavLink to="/contact"><button className="button-homepage button-homepage-anime">ME CONTACTER</button></NavLink>
+    
+    
+    </div>
   )
 }
