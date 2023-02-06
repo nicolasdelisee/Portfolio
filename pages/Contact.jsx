05f485af14de1358b273ea303187c3d09262c89b
nicolasdelisee <nicolas.delisee3@gmail.com>
2023-02-06 17:01:36 +0100

    Contact form almost finished

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./contact.css";
 import cv from "../src/assets/pictures/cv.jpg";
 import { gsap } from "gsap";
@@ -20,34 +21,54 @@ const Contact = () => {
       setIcon("close-circle");
     }
   };
-    const downloadPDF = () => {
-      // using Java Script method to get PDF file
-      fetch('Nicolas Delisée - CV.pdf').then(response => {
-          response.blob().then(blob => {
-              // Creating new object of PDF file
-              const fileURL = window.URL.createObjectURL(blob);
-              // Setting various property values
-              let alink = document.createElement('a');
-              alink.href = fileURL;
-              alink.download = 'Nicolas Delisée - CV.pdf';
-              alink.click();
-          })
-      })
-  }
+  const downloadPDF = () => {
+    // using Java Script method to get PDF file
+    fetch("Nicolas Delisée - CV.pdf").then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "Nicolas Delisée - CV.pdf";
+        alink.click();
+      });
+    });
+  };
 
-useEffect(() => {
-gsap.fromTo(".form__pdf", {x: -800, duration: 3}, { x: 0, duration: 3 });
-gsap.fromTo(".form__contact", {x: 800, duration: 3}, { x: 0, duration: 3});
-}, []);
+  useEffect(() => {
+    gsap.fromTo(".form__pdf", { x: -800, duration: 3 }, { x: 0, duration: 3 });
+    gsap.fromTo(
+      ".form__contact",
+      { x: 800, duration: 3 },
+      { x: 0, duration: 3 }
+    );
+  }, []);
 
   return (
     <div className="form">
       <div className="form__pdf">
         <img className="pdf" src={cv} alt="CV" />
-        <button className="pdf__button" onClick={downloadPDF}>Télécharger le CV</button>
+        <button className="pdf__button" onClick={downloadPDF}>
+          Télécharger le CV
+        </button>
       </div>
       <div className="form__contact">
         <div className="form__title">
+          <h2>Mes coordonnées</h2>
+          <h3>07 69 92 73 99</h3>
+          <a href="mailto:nicolas.delisee3@gmail.com" target="_blank">
+            <h3>nicolas.delisee3@gmail.com</h3>
+          </a>
+          <a href="https://github.com/nicolasdelisee" target="_blank">
+            <h3>GitHub/nicolasdelisee</h3>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nicolas-delis%C3%A9e-2b290b42/"
+            target="_blank"
+          >
+            <h3>LinkedIn/nicolasdelisee</h3>
+          </a>
           <h2>Contact</h2>
         </div>
         <form className="form__div">
