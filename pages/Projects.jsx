f8a8d3de85b94852a529e2a6107c07e7f95bdd1d
nicolasdelisee <nicolas.delisee3@gmail.com>
2023-02-09 11:35:45 +0100

    OK, almost finished

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import Project from '../data/Project';
+import React from "react";
+import Project from "../data/Project";
 import "./projects.scss";
 
 const Projects = () => {
-    return (
-        <div id="timeline">
-            {Project.map((e) => (
-                 <div className="tl-item">
-                    <div className="tl-bg"><img src={e.url_image} alt={e.name} className={`tl-bg ${e.classname_image}`}/></div>
-                    <div className="tl-type">{e.type}
-                    </div>
-                    <div className="tl-content">
-                        <h1>{e.name}</h1>
-                        <h5>({e.duration})</h5>
-                        <p>{e.description}</p>
-                    </div>
-                </div>
-            ))}
-
-
+  return (
+    <div id="timeline">
+      {Project.map((e) => (
+        <div className="tl-item">
+          <div className="tl-bg">
+            <img
+              src={e.url_image}
+              alt={e.name}
+              className={`tl-bg ${e.classname_image}`}
+            />
+          </div>
+          <div className="tl-type">
+            <h4>{e.type}</h4>
+            <h3>{e.name}</h3>
+          </div>
+          <div className="tl-content">
+            <h5>Durée : {e.duration}</h5>
+            <p>{e.description}</p>
+          </div>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default Projects;
